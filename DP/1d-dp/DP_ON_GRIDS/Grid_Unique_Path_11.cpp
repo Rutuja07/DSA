24cc5803cef6e4ed4009e25d862894f1225cb954
Rutuja07 <rutujadabade799@gmail.com>
2025-06-03 19:13:03 +0530

    Grid Unique path II
    Signed-off-by: Rutuja07 <rutujadabade799@gmail.com>

---
+++ DP/1d-dp/DP_ON_GRIDS/Grid_Unique_Path_11.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
+        if(matrix[0][0] == 1)
+            return 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m,0));
+        dp[0][0] = 1;
+        for(int i = 1; i<m ;i++ ){
+            if(matrix[0][i] == 0 && dp[0][i-1] == 1 ){
+              dp[0][i] = 1;
+            }
+        }
+        for(int i = 1; i<n ;i++ ){
+            if(matrix[i][0] == 0 && dp[i-1][0] == 1 ){
+              dp[i][0] = 1;
+            }
+        }
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m;j++){
+            if(matrix[i][j] == 0){
+               dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+            }
+        }
+
+
+        return dp[n-1][m-1];
+    }
+};
